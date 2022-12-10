d8e2f6a6fa478f77704e3bfac3760a7fc8a8ea82
advanc3dUA <fash1k@gmail.com>
2022-12-10 13:56:41 +0100

    Adds main logic to FlowerLogic
    - adds Alamofire request
    - adds JSON parsing with SwiftyJSON
    - Prints pageID of requested flower
    - Prints Description of requested flower

--- WhatFlower/FlowerLogic.swift
+++ WhatFlower/FlowerLogic.swift
@@ -11,13 +11,40 @@ import SwiftyJSON
 
 struct FlowerLogic {
     
-    mutating func parseJSON(for flower: String) {
+    func parseJSON(for flower: String) {
         guard let flowerURL = createURL(with: flower) else {
             fatalError("Unable to create valid URL with flower")
         }
+        
+        AF.request(flowerURL).response { response in
+            guard let result = response.data else {
+                fatalError("Couldn't get response.data with Alamofire")
+            }
+            self.parse1JSON(with: result)
+        }
     }
     
-    fileprivate func createURL(with flower: String) -> URL? {
+    func parse1JSON(with data: Data) {
+        guard let json = try? JSON(data: data) else {
+            fatalError("Couldn't get JSON while parsing the result")
+        }
+        
+        let pathToPageID: [JSONSubscriptType] = ["query", "pageids"]
+        guard let pageID = json[pathToPageID].first?.1.stringValue else {
+            fatalError("Couldn't get pageID")
+        }
+        
+        print(pageID)
+        
+        let pathToDescription: [JSONSubscriptType] = ["query", "pages", pageID, "extract"]
+        let description = json[pathToDescription].stringValue
+
+        print(description)
+        
+    }
+    
+    
+    fileprivate func createURL(with flower: String) -> String? {
         let wikiURL = "https://en.wikipedia.org/w/api.php"
         let flowerName = flower.replacingOccurrences(of: " ", with: "%20")
         
@@ -36,7 +63,7 @@ struct FlowerLogic {
         for parameter in parameters {
             flowerURLString += parameter.key + "=" + parameter.value + "&"
         }
-        print("String =", flowerURLString)
-        return URL(string: flowerURLString)
+        
+        return flowerURLString
     }
 }
